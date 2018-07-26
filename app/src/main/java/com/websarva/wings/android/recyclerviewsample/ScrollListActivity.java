2ef2678c85cdc254442714858592151640924765
kishimotsu72 <kishimotsu72@gmail.com>
2018-07-26 10:02:35 +0900

    最初

---
+++ app/src/main/java/com/websarva/wings/android/recyclerviewsample/ScrollListActivity.java
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.recyclerviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScrollListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_list);
+    }
+}
